d121fb4b25ac3dd2f2bae5f96038c34fca4fe7a9
python290821 <python290821@gmail.com>
2022-05-22 18:30:00 +0300

    upload all

---
+++ calc.js
@@ -0,0 +1,13 @@
+const calc = {};
+
+calc.add = (num1, num2) => {
+    return num1 + num2;
+}
+
+calc.multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+module.exports = calc;
+
+
---
+++ calc.test.js
@@ -0,0 +1,20 @@
+
+const calc = require('./calc');
+const assert = require('assert')
+const expect = require('chai').expect
+
+describe('Test calculator basics', () => {
+    it('basic add 1 + 1 == 2', () => {
+        actual_result = calc.add(1, 1);
+        expected_result = 2;
+        assert.strictEqual(actual_result, expected_result)
+    });
+})
+
+describe('Test calculator basics with chai', () => {
+    it('basic mul 3 * 4 == 12', () => {
+        actual_result = calc.multiply(3, 4)
+        expected_result = 12;
+        expect(expected_result).to.equal(actual_result)
+    });
+})
